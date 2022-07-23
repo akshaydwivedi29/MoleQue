954d95099daad869f68e706acd3508df64391274
sumitpcsa <sumitpcsa@gmail.com>
2022-07-23 19:17:53 +0530

    basic structure ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { TestDetailsComponent } from './test-details/test-details.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomepageComponent
-  }
+    component: HomepageComponent,
+  },
+  {
+    path: 'test-details',
+    component: TestDetailsComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { TestsService, TestList } from './tests.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private testsService: TestsService) {}
 
-  constructor() { }
+  testlist: Array<TestList> = [];
+  hasQuery: Boolean = false;
 
-  ngOnInit(): void {
+  sendData(event: any) {
+    let query: string = event.target.value;
+    //Will match if query is nothing or is only spaces
+    let matchSpaces: any = query.match(/\s*/);
+    if (matchSpaces[0] === query) {
+      this.testlist = [];
+      this.hasQuery = false;
+      return;
+    }
+
+    this.testsService
+      .searchTestList(query.trim())
+      .subscribe((results: TestList[]) => {
+        this.testlist = results;
+        this.hasQuery = true;
+        console.log(results);
+      });
   }
 
+  menuVariable: boolean = false;
+  menu_icon_variable: boolean = false;
+  openMenu() {
+    this.menuVariable = !this.menuVariable;
+    this.menu_icon_variable = !this.menu_icon_variable;
+  }
+
+  ngOnInit(): void {}
 }
